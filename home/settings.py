9e6903606b81ebccb6a2bf76b2460504843a23ae
HoangPer007 <22130088@st.hcmuaf.edu.vn>
2025-05-21 16:03:29 +0700

    convert DB to postgreSQL

--- home/settings.py
+++ home/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 import os
 from pathlib import Path
+import dj_database_url
+
+# postgresql://web_disease_db_user:JFWCKYPZ8BlsXZW1vzpQbXoixliOxDoi@dpg-d0mp2rre5dus738nfcr0-a.oregon-postgres.render.com/web_disease_db
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,11 +81,21 @@ WSGI_APPLICATION = 'home.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# Database sqlite3
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+# Database Postgres
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True  # cần cho Render
+    )
 }
 
 
